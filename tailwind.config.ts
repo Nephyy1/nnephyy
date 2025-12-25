6dc38fd57ae93e4e2c99164233d9bd9aaf2358a6
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-12-26 06:40:15 +0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#FAF9F6",
+        main: "#FF6B00",
+        accent: "#C4A1FF",
+        dark: "#050505",
+      },
+      boxShadow: {
+        neo: "4px 4px 0px 0px rgba(0,0,0,1)",
+        neo_sm: "2px 2px 0px 0px rgba(0,0,0,1)",
+      },
+      fontFamily: {
+        sans: ["var(--font-space-grotesk)"],
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
